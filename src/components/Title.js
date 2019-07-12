77b5b4e61790135c70d61da59cad4636b14f7409
Y2dgWLzz1T <seungjin.public@gmail.com>
2019-07-12 15:26:18 +0900

    App 제목 컴포넌트

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+/** 제목 컴포넌트(단순 이미지 표시용 컴포넌트) */
+export const Title = () => {
+  return (
+    <span>
+      <img src="https://firebasestorage.googleapis.com/v0/b/iamdevbang.appspot.com/o/common%2Fmydogbingo.png?alt=media&token=a943d53a-2801-45ea-8510-dcd45e273b23" alt="우리집 강아지는 빙고" /> 
+    </span>
+  )
+}
