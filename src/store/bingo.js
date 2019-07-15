8295b2dd4c543968bb7e6d6947206032795558e0
Y2dgWLzz1T <seungjin.public@gmail.com>
2019-07-15 19:12:08 +0900

    store 간소화 및 변경

--- src/store/bingo.js
+++ src/store/bingo.js
@@ -1,4 +1,7 @@
 /**  BINGO Reducer Action Types */
+const ACTION_START_FLAG = 'ACTION_START_FLAG'; 
+const ACTION_RESTART_FLAG = 'ACTION_RESTART_FLAG'; 
+
 const CHANGE_GAME_STATUS     = 'CHANGE_GAME_STATUS'; 
 const RESTART_GAME_STATUS    = 'RESTART_GAME_STATUS'; 
 const CHANGE_SELECTED_NUMBER = 'CHANGE_SELECTED_NUMBER'; 
@@ -8,6 +11,9 @@ const RESET_STATUS           = 'RESET_STATUS';
 const CHANGE_RESTART_STATUS  = 'CHANGE_RESTART_STATUS'; 
 
 /** BINGO Action Creators */
+export const actionStartFlag = () => { return { type: ACTION_START_FLAG }}; 
+export const actionRestartFlag = () => { return { type: ACTION_RESTART_FLAG }}; 
+
 export function changeGameStatus(status) {
   return {
     type: CHANGE_GAME_STATUS, 
@@ -58,63 +64,23 @@ export function resetStatus() {
 
 /** Bingo Store 초기 상태 */
 const initState = {
-  isRunning: false, // 게임 진행 여부 
-  nowPlayer: 1, // 현재 유저 순서 
-  selectedNumber: null, // 최근 Cell 선택 번호 
-  point : { // 플레이어 (1/2P)간 빙고 성공 개수 
-    "1": null, 
-    "2": null, 
-  }, 
-  isRestart: false, // 게임 재시작 여부 Flag 
+  isRunning: false, 
+  isRestart: false, 
 }; 
 
 /** Bingo Reducers */
 export default function bingo(state = initState, action) {
   switch(action.type) {
-    case CHANGE_GAME_STATUS:
+    case ACTION_START_FLAG: 
       return {
         ...state, 
-        nowPlayer: 1, 
-        isRunning: action.status,
-        isRestart: true,  
-      }; 
-    case RESTART_GAME_STATUS: 
-      return {
-        ...state, 
-        nowPlayer: 1, 
-        selectedNumber: [], 
-        point: {
-          "1": null, 
-          "2": null, 
-        }, 
-        isRunning: true, 
-        isRestart: true, 
-      } 
-    case CHANGE_RESTART_STATUS: 
-       return {
-         ...state, 
-         isRestart: action.status,   
-       }
-    case CHANGE_SELECTED_NUMBER: 
-      return {
-        ...state, 
-        selectedNumber: action.value, 
-      }; 
-    case CHANGE_PLAYER_TURN: 
-      return {
-        ...state, 
-        nowPlayer: action.player
-      };
-    case CHANGE_PLAYER_POINT: 
-      return {
-        ...state, 
-        point: {
-          ...state.point, 
-          [action.info.player]: action.info.count 
-        }
+        isRunning: !action.flag, 
       }
-    case RESET_STATUS: 
-      return initState; 
+    case ACTION_RESTART_FLAG: 
+      return {
+        ...state, 
+        isRestart: !action.flag, 
+      }
     default: 
       return state; 
   }
